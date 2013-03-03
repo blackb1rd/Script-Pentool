9bbfe3ea2de91d3fb0bc29d8cd8595cf15e4cd86
rainb0wb1rd <b1rd@R-b1rd.(none)>
2013-03-03 22:08:21 +0700

    update script and fix bugs

--- update.sh
+++ update.sh
@@ -1,35 +1,73 @@
 #! /bin/bash
-echo -e "${txtbld}$(tput setaf 1)[-] Updating Exploit-db, please wait...$(tput sgr0)"
+
+#Check directory
+# ->/pentest
+# |->exploits
+#  |->exploitdb
+#  |->set
+#  |->msf
+# |->web
+#  |->nikto
+# |->databse
+#  |->sqlmap
+
+if [ ! -d "/pentest" ]; then
+	mkdir "/pentest"
+	mkdir "/pentest/exploits"
+	mkdir "/pentest/exploits/exploitdb"
+	mkdir "/pentest/exploits/exploitdb/platforms"
+	mkdir "/pentest/exploits/set"
+	mkdir "/pentest/web"
+	mkdir "/pentest/web/nikto"
+	mkdir "/pentest/database"
+	mkdir "/pentest/database/sqlmap"
+fi
+
+#Update
+echo "${txtbld}$(tput setaf 1)[-] Updating Exploit-db, please wait...$(tput sgr0)"
 exploit=$(svn co svn://www.exploit-db.com/exploitdb exploitdb)
 wait
-if [[$exploit =~ "Can't connect to host"]] ;then 
-echo -e "${txtbld}$(tput setaf 1)[!]Exploit-DB's SVN didn't work. Updating manually. Please wait...\n"
+if [ -z "$exploit" ]; then 
+echo "${txtbld}$(tput setaf 1)[!]Exploit-DB's SVN didn't work. Updating manually. Please wait...\n"
 wget http://www.exploit-db.com/archive.tar.bz2
 wait
-tar -xvfj archive.tar.bz2 > /dev/null
-rm -rf /pentest/exploits/exploitdb/platforms/
-mv -f platforms/ files.csv /pentest/exploits/exploitdb/
+tar xvfj archive.tar.bz2 > /dev/null
+rm -rf /pentest/exploits/exploitdb/platforms
+mv -f platforms files.csv /pentest/exploits/exploitdb
 rm -rf archive.tar.bz2*
 fi
-echo -e "${txtbld}$(tput setaf 4)[>] Exploit-db updated successfully!$(tput sgr0)\n"
+echo "${txtbld}$(tput setaf 4)[>] Exploit-db updated successfully!$(tput sgr0)\n"
 
-echo -e "${txtbld}$(tput setaf 1)[-] Updating SET, please wait...$(tput sgr0)"
+echo "${txtbld}$(tput setaf 1)[-] Updating SET, please wait...$(tput sgr0)"
+if [ ! -f "/pentest/exploits/set/set-update" ]; then
+	cd /pentest/exploits/set
+	svn co http://svn.secmaniac.com/social_engineering_toolkit set/
+fi
 /pentest/exploits/set/set-update
 wait
-echo -e "${txtbld}$(tput setaf 4)[>] SET updated successfully!$(tput sgr0)\n"
+echo "${txtbld}$(tput setaf 4)[>] SET updated successfully!$(tput sgr0)\n"
 
-echo -e "${txtbld}$(tput setaf 1)[-] Updating msf, please wait...$(tput sgr0)"
+echo "${txtbld}$(tput setaf 1)[-] Updating msf, please wait...$(tput sgr0)"
+if [ ! -f "/pentest/exploits/msf/msfupdate" ]; then
+	cd /pentest/exploits/
+	svn co https://www.metasploit.com/svn/framework3/trunk/ msf
+fi
 /pentest/exploits/msf/msfupdate
 wait
-echo -e "${txtbld}$(tput setaf 4)[>] msf updated successfully!$(tput sgr0)\n"
+echo "${txtbld}$(tput setaf 4)[>] msf updated successfully!$(tput sgr0)\n"
 
-echo -e "${txtbld}$(tput setaf 1)[-] Updating Nikto, please wait...$(tput sgr0)"
-svn co http://svn2.assembla.com/svn/Nikto_2/trunk /pentest/web/nikto/
+echo "${txtbld}$(tput setaf 1)[-] Updating Nikto, please wait...$(tput sgr0)"
+if [ -d "/pentest/web/nikto" ]; then
+	rm -r "/pentest/web/nikto"
+fi
+git clone git://github.com/sullo/nikto.git /pentest/web/nikto
 wait
-echo -e "${txtbld}$(tput setaf 4)[>] Nikto updated successfully!$(tput sgr0)\n"
+echo "${txtbld}$(tput setaf 4)[>] Nikto updated successfully!$(tput sgr0)\n"
 
 
-echo -e "${txtbld}$(tput setaf 1)[-] Updating SQLMap, please wait...$(tput sgr0)"
+echo "${txtbld}$(tput setaf 1)[-] Updating SQLMap, please wait...$(tput sgr0)"
 git clone https://github.com/sqlmapproject/sqlmap.git /pentest/database/sqlmap
+cd /pentest/database/sqlmap
+git pull
 wait
 echo -e "${txtbld}$(tput setaf 4)[>] SQLMap updated successfully!$(tput sgr0)\n"
