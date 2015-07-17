22ec85dceedcf38fe0de31852767f3b0c3036e69
blackb1rd <blackb1rd.mov@gmail.com>
2015-07-17 22:34:49 +0700

    Refactor and cleanup

--- update.sh
+++ update.sh
@@ -9,6 +9,22 @@
 #         └>web───────┬>nikto
 #                     └>w3af
 
+
+gitpull () {
+    cd "$1" && git pull -v
+}
+
+github () {
+    echo "${txtbld}$(tput setaf 1)[-] Updating $3, please wait...$(tput sgr0)"
+    if [ ! -d "$2" ]; then
+        git clone "https://github.com/$1.git" "$2"
+    else
+        gitpull $1
+    fi
+    wait
+    echo "${txtbld}$(tput setaf 4)[>] $3 updated successfully!$(tput sgr0)"
+}
+
 if [ ! -d "/pentest" ]; then
 	mkdir "/pentest"
 fi
@@ -25,75 +41,10 @@ if [ ! -d "/pentest/web" ]; then
 	mkdir "/pentest/web"
 fi
 
-#Update exploit-db
-echo "${txtbld}$(tput setaf 1)[-] Updating Exploit-db, please wait...$(tput sgr0)"
-if [ ! -d "/pentest/exploits/exploitdb" ]; then
-    git clone https://github.com/offensive-security/exploit-database /pentest/exploits/exploitdb
-else
-    cd /pentest/exploits/exploitdb && git pull
-fi
-wait
-echo "${txtbld}$(tput setaf 4)[>] Exploit-db updated successfully!$(tput sgr0)"
-
-
-#Update SET
-echo "${txtbld}$(tput setaf 1)[-] Updating SET, please wait...$(tput sgr0)"
-if [ ! -d "/pentest/exploits/set" ]; then
-    git clone https://github.com/trustedsec/social-engineer-toolkit.git /pentest/exploits/set
-else
-    cd /pentest/exploits/set && git pull
-fi
-wait
-echo "${txtbld}$(tput setaf 4)[>] SET updated successfully!$(tput sgr0)"
-
-#Update msf
-echo "${txtbld}$(tput setaf 1)[-] Updating msf, please wait...$(tput sgr0)"
-if [ ! -d "/pentest/exploits/msf" ]; then
-    git clone https://github.com/rapid7/metasploit-framework.git /pentest/exploits/msf
-else
-    cd /pentest/exploits/msf && git pull
-fi
-wait
-echo "${txtbld}$(tput setaf 4)[>] msf updated successfully!$(tput sgr0)"
-
-#Update SQLMap
-echo "${txtbld}$(tput setaf 1)[-] Updating SQLMap, please wait...$(tput sgr0)"
-if [ ! -d "/pentest/database/sqlmap" ]; then
-    git clone https://github.com/sqlmapproject/sqlmap.git /pentest/database/sqlmap
-else
-    cd /pentest/database/sqlmap && git pull
-fi
-wait
-echo "${txtbld}$(tput setaf 4)[>] SQLMap updated successfully!$(tput shr0)"
-
-#Update pwnwiki
-echo "${txtbld}$(tput setaf 1)[-] Updating Pwnwiki, please wait...$(tput sgr0)"
-if [ ! -d "/pentest/tutorial/pwnwiki" ]; then
-    git clone https://github.com/pwnwiki/pwnwiki.github.io.git /pentest/tutorial/pwnwiki
-else
-    cd /pentest/tutorial/pwnwiki && git pull
-fi
-wait
-echo "${txtbld}$(tput setaf 4)[>] Pwnwiki updated successfully!$(tput sgr0)"
-
-#Update Nikto
-echo "${txtbld}$(tput setaf 1)[-] Updating Nikto, please wait...$(tput sgr0)"
-if [ ! -d "/pentest/web/nikto" ]; then
-    git clone git://github.com/sullo/nikto.git /pentest/web/nikto
-else
-    cd /pentest/web/nikto && git pull
-fi
-
-wait
-echo "${txtbld}$(tput setaf 4)[>] Nikto updated successfully!$(tput sgr0)"
-
-#Update w3af
-echo "${txtbld}$(tput setaf 1)[-] Updating w3af, please wait...$(tput sgr0)"
-if [ ! -d "/pentest/web/w3af" ]; then
-    git clone https://github.com/andresriancho/w3af.git /pentest/web/w3af
-else
-    cd /pentest/web/w3af && git pull
-fi
-wait
-echo "${txtbld}$(tput setaf 4)[>] w3af updated successfully!$(tput sgr0)"
-
+github offensive-security/exploit-database /pentest/exploits/exploitdb Exploitdb
+github trustedsec/social-engineer-toolkit /pentest/exploits/set SET
+github rapid7/metasploit-framework /pentest/exploits/msf MSF
+github sqlmapproject/sqlmap /pentest/database/sqlmap SQLMap
+github pwnwiki/pwnwiki.github.io /pentest/tutorial/pwnwiki PwnWiki
+github sullo/nikto /pentest/web/nikto Nikto
+github andresriancho/w3af /pentest/web/w3af w3af
